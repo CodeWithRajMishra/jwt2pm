ab0e7eb70696a2ef328236f55f680236280fa939
CodeWithRajMishra <rajmishra3@gmail.com>
2025-02-24 16:28:38 +0530

    doctor

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+
+const Footer=()=>{
+    return(
+        <>
+          <div id="footer">
+           www.mydoctor.con
+          </div>
+        
+        </>
+    )
+}
+
+
+export default Footer;
